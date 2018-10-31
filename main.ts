374cdaeedeea46d35293118e53acc5d2056e421d
lupyuen <luppy@appkaki.com>
2018-10-31 17:20:33 +0800

    Updated blocks

--- main.ts
+++ main.ts
@@ -1,22 +1,23 @@
+let network_task_id = 0
 let tmp = 0
-let sensorMsg: sigfox.SensorMsg = null
 let beginMsg: sigfox.SensorMsg = null
+let sensorMsg: sigfox.SensorMsg = null
 beginMsg = sigfox.createSensorMsg(sigfox.BEGIN_SENSOR_NAME, 0)
 sensorMsg = sigfox.createSensorMsg("tmp", 23.4)
 // Erase the aggregated sensor data.
 sigfox.setup_aggregate()
-const network_task_id = sigfox.network_setup()
+network_task_id = sigfox.network_setup()
 sigfox.msg_post(network_task_id, beginMsg)
 basic.pause(20 * 1000)
 sigfox.msg_post(network_task_id, sensorMsg)
 basic.forever(function () {
-
+	
 })
 control.inBackground(function () {
     while (true) {
         tmp = input.temperature()
         sensorMsg = sigfox.createSensorMsg("tmp", tmp)
         sigfox.msg_post(network_task_id, sensorMsg)
-        basic.pause(20 * 1000)
+basic.pause(20 * 1000)
     }
 })
