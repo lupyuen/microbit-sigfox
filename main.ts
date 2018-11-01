3b989de06837277cbcbdc6401e5e00f3d9e09aa4
lupyuen <luppy@appkaki.com>
2018-11-01 11:48:51 +0800

    Tested OK

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
 let lig = 0
 let cmp = 0
 let tmp = 0
-sigfox.setupSigfox(
-    sigfox.Country.COUNTRY_SG,
-    ["tmp", "lig", "cmp"]
-)
+sigfox.setupSigfox(sigfox.Country.COUNTRY_SG, ["tmp", "lig", "cmp"])
 basic.forever(function () {
 	
 })
