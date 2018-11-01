338eafaffc08938bfa7be298e24806c87f119178
lupyuen <luppy@appkaki.com>
2018-11-01 11:36:07 +0800

    Tested OK

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
+let lig = 0
+let cmp = 0
+let tmp = 0
 sigfox.setupSigfox(["tmp", "lig", "cmp"])
+basic.forever(function () {
+	
+})
 control.inBackground(function () {
     while (true) {
-        const tmp = input.temperature()
+        tmp = input.temperature()
         sigfox.sendToSigfox("tmp", tmp)
         basic.pause(20000)
     }
 })
 control.inBackground(function () {
     while (true) {
-        const lig = input.lightLevel()
-        sigfox.sendToSigfox("lig", lig)
+        cmp = input.compassHeading()
+        sigfox.sendToSigfox("cmp", cmp)
         basic.pause(20000)
     }
 })
 control.inBackground(function () {
     while (true) {
-        const cmp = input.compassHeading()
-        sigfox.sendToSigfox("cmp", cmp)
+        lig = input.lightLevel()
+        sigfox.sendToSigfox("lig", lig)
         basic.pause(20000)
     }
-})
-basic.forever(function () {
-	
 })
