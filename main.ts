0035983c5321f6b0e8b32a7a57e7da053c379a77
lupyuen <luppy@appkaki.com>
2018-11-01 11:07:34 +0800

    Added light, compass sensors

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
+let sendSensors: string[] = []
 let tmp = 0
+let cmpMsg: sigfox.Msg_t = null
+let ligMsg: sigfox.Msg_t = null
+let tmpMsg: sigfox.Msg_t = null
 let network_task_id = 0
 let beginMsg: sigfox.Msg_t = null
 let sensorMsg: sigfox.Msg_t = null
+tmp = 0
+led.enable(false)
 beginMsg = sigfox.createSensorMsg(sigfox.BEGIN_SENSOR_NAME, 0)
 sensorMsg = sigfox.createSensorMsg("tmp", 23.4)
 // Erase the aggregated sensor data.
-const sendSensors = ["tmp", "hmd", "alt"];  //  Sensors to be sent.
+sendSensors = ["tmp", "lig", "cmp"]
 sigfox.setup_aggregate(sendSensors)
 network_task_id = sigfox.network_setup()
 sigfox.msg_post(network_task_id, beginMsg)
@@ -16,9 +22,22 @@ basic.forever(function () {
 })
 control.inBackground(function () {
     while (true) {
-        tmp = input.temperature()
-        sensorMsg = sigfox.createSensorMsg("tmp", tmp)
-        sigfox.msg_post(network_task_id, sensorMsg)
-basic.pause(20 * 1000)
+        tmpMsg = sigfox.createSensorMsg("tmp", input.temperature())
+        sigfox.msg_post(network_task_id, tmpMsg)
+basic.pause(20000)
+    }
+})
+control.inBackground(function () {
+    while (true) {
+        ligMsg = sigfox.createSensorMsg("lig", input.lightLevel())
+        sigfox.msg_post(network_task_id, ligMsg)
+basic.pause(20000)
+    }
+})
+control.inBackground(function () {
+    while (true) {
+        cmpMsg = sigfox.createSensorMsg("cmp", input.compassHeading())
+        sigfox.msg_post(network_task_id, cmpMsg)
+basic.pause(20000)
     }
 })
