0c79ec39ee67b9aa3ef828716c45ec5d9e05c64e
lupyuen <luppy@appkaki.com>
2018-11-04 03:04:42 +0800

    Enable LED for light sensor

--- custom.ts
+++ custom.ts
@@ -1,17 +1,23 @@
 namespace sigfox {
     //% block
-    export let network_task_id = 0
+    export let network_task_id = 0  //  To transmit a Sigfox message, post the message to this task ID.
 
     //% block
     export function setupSigfox(
         country: Country,
         sendSensors: string[]
     ): void {
+        //  Initialise the Sigfox Wisol module and start the Network Task and UART Task to
+        //  send and receive Sigfox messages.
+
         //  TODO: Validate sendSensors.
         //  TODO: Simulator mode
 
-        //  Disable the LED because it may interfere with other sensors.
-        led.enable(false)
+        //  If using the light sensor, enable the LED.
+        led.enable(true)
+
+        //  If not using the light sensor, disable the LED because it may interfere with other sensors.
+        //  led.enable(false)
 
         //  Set serial port to USB for console output.
         serial.redirectToUSB()
@@ -19,22 +25,18 @@ namespace sigfox {
         //  Erase the aggregated sensor data.
         setup_aggregate(sendSensors)
 
-        //  Start the Network Task to send and receive network messages.
+        //  Start the Network Task and UART Task to send and receive Sigfox messages.
         network_task_id = network_setup(country)
-        ////debug_println("setupSigfox")  ////
 
         //  Initialise the Wisol module.  Must be done in background or the program will pause forever.
         control.inBackground(function () {
             //  Initialise the Wisol module. 
             const msg = createSensorMsg(sigfox.BEGIN_SENSOR_NAME, 0)
-            ////debug_println("setupSigfox 1")  ////
             msg_post(network_task_id, msg)
-            ////debug_println("setupSigfox 2")  ////
         })
 
         //  Wait 7 seconds for Wisol module to be initialised.
         basic.pause(7 * 1000)
-        ////debug_println("setupSigfox: Done")  ////
     }
 
     //% block
@@ -42,6 +44,8 @@ namespace sigfox {
         name: string,
         value: number
     ): void {
+        //  Send the sensor name and value to Sigfox.  The value will be aggregated before sending.
+        //  See aggregate.ts.
         const msg = createSensorMsg(name, value)
         msg_post(network_task_id, msg)
     }
@@ -60,12 +64,8 @@ namespace sigfox {
             uartResponse);
         const uartContextWrapped = <Context_t>{ uartContext: uartContext };
         const uartTaskID = task_create(
-            uart_task,     //  Task will run this function.
-            uartContextWrapped,  //  task_get_data() will be set to the display object.
-            // 10,            //  Priority 10 = highest priority
-            // (Msg_t *) uartMsgPool,  //  Pool to be used for storing the queue of UART messages.
-            // UART_MSG_POOL_SIZE,     //  Size of queue pool.
-            // sizeof(UARTMsg)         //  Size of queue message.
+            uart_task,          //  Task will run this function.
+            uartContextWrapped  //  Context for the UART Task.
         );
 
         // Start the Network Task for receiving sensor data
@@ -78,12 +78,8 @@ namespace sigfox {
             false);
         const wisolContextWrapped = <Context_t>{ networkContext: wisolContext };
         const networkTaskID = task_create(
-            network_task,   //  Task will run this function.
-            wisolContextWrapped,  //  task_get_data() will be set to the display object.
-            // 20,             //  Priority 20 = lower priority than UART task
-            // (Msg_t *) networkMsgPool,  //  Pool to be used for storing the queue of UART messages.
-            // NETWORK_MSG_POOL_SIZE,     //  Size of queue pool.
-            // sizeof(SensorMsg)   //  Size of queue message.
+            network_task,        //  Task will run this function.
+            wisolContextWrapped  //  Context for the Network Task.
         );
 
         set_uart_task_id(uartTaskID);  //  Remember the UART Task ID for sending debug messages.
@@ -92,6 +88,7 @@ namespace sigfox {
 
     let uartResponse: string = null
 
+    //  Context for the UART Task.
     let uartContext = <UARTContext>{
         status: false,
         sendIndex: 0,
@@ -101,6 +98,8 @@ namespace sigfox {
         testTimer: 0,
         msg: null,
     }
+
+    //  Context for the Network Task.
     let wisolContext = <NetworkContext>{
         uartContext: null,
         uartTaskID: 0,
