962f49e837da887fc3a7e803b17b5720e30f2811
lupyuen <luppy@appkaki.com>
2018-11-03 17:01:26 +0800

    Buffer debug output

--- platform.ts
+++ platform.ts
@@ -6,9 +6,11 @@ namespace sigfox {
     export const MAX_UART_RESPONSE_MSG_SIZE: number = 36
 
     let uartTaskID = 0
+    let debugBuffer: string[] = []
+    const MAX_DEBUG_BUFFER_SIZE = 10
+    
     //  TODO
     export function led_toggle() { }
-    export function debug_flush() { }
     export function millis(): int32 {
         //  Number of seconds elapsed since power on.
         return input.runningTime()
@@ -17,18 +19,33 @@ namespace sigfox {
 
     export function debug(p1: string, p2: string = null): void {
         debug_println(p1, p2);
+        debug_flush();
     }
-    export function debug_println(p1: string, p2: string = null): void {
-        const s = p1 + ((p2 === null) ? "" : p2);
-        uart_write(s + "\n");
-    }
-    export function debug_print(p1: string, p2: string = null): void {
-        const s = p1 + ((p2 === null) ? "" : p2);
+
+    export function debug_flush(): void {
+        if (debugBuffer.length == 0) { return; }
+        const s = debugBuffer.join("");
+        debugBuffer = [];
         uart_write(s);
     }
+
+    export function debug_print(p1: string, p2: string = null): void {
+        const s = p1 + ((p2 === null) ? "" : p2);
+        debugBuffer.push(s);
+        if (debugBuffer.length >= MAX_DEBUG_BUFFER_SIZE) {
+            debug_flush();
+        }
+    }
+
+    export function debug_println(p1: string, p2: string = null): void {
+        const s = p1 + ((p2 === null) ? "" : p2);
+        debug_print(s + "\n");
+    }
+
     export function set_uart_task_id(task_id: number): void {
         uartTaskID = task_id;
     }
+
     function uart_write(s: string): void {
         if (uartTaskID === 0) {
             serial.writeLine("***** ERROR: uart_write / missing UART task ID");
